0a62068277446e10592bc6d03a9d1f3f41fe1463
vaibhavvTripathi <github.vaibhavtripathi@gmail.com>
2023-11-30 12:51:37 +0530

    added search in user panel

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import EditData from "./EditData";
-import {AiFillDelete, AiTwotoneEdit} from 'react-icons/ai'
+import { AiFillDelete, AiTwotoneEdit } from "react-icons/ai";
 
-const DataTable = ({ actualData, fields, editableFields, delete_API, action, edit_API,showData,delete_func,edit_func
+const DataTable = ({
+  actualData,
+  fields,
+  editableFields,
+  delete_API,
+  action,
+  edit_API,
+  showData,
+  delete_func,
+  edit_func,
 }) => {
   // Set the Data to be actual when no show data is provided
   let Data = showData === undefined ? actualData : showData;
@@ -12,38 +21,52 @@ const DataTable = ({ actualData, fields, editableFields, delete_API, action, edi
     isEdit: false,
     index: null,
   });
-  
+
   // const handleEdit = (e) => {
   // setEditData({ ...editData, [e.target.name]: e.target.value });
   // };
 
+  const [query, setQuery] = useState("");
+
   if (edit.isEdit) {
     return (
-      <EditData editableFields={editableFields} fields={fields} index={edit.index} isEdit={edit.isEdit} edit_API={edit_API} action={action} setEdit={setEdit} edit_func={edit_func}/>
-    )
+      <EditData
+        editableFields={editableFields}
+        fields={fields}
+        index={edit.index}
+        isEdit={edit.isEdit}
+        edit_API={edit_API}
+        action={action}
+        setEdit={setEdit}
+        edit_func={edit_func}
+      />
+    );
   }
   return (
     <div className="  h-100 mt-1 vh-100">
       {/* Search Component  */}
       <div className="row mt-4 mb-3 d-flex">
         <div className="col-sm-12 align-items-center d-flex ">
-
           <input
             type="text"
             name="searchText"
             className="form-control shadow-lg p-2 px-3"
-            placeholder="Search here"
+            placeholder="Search names here.."
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
-            style={{ borderRadius: "20px", backgroundColor: '#5D5D5D', color: '#FFF' }}
+            style={{
+              borderRadius: "20px",
+              backgroundColor: "#5D5D5D",
+              color: "#FFF",
+            }}
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
           />
         </div>
       </div>
 
       {/* Data Table */}
-      <div
-        className="overflow-auto"
-      >
+      <div className="overflow-auto">
         <table className=" table" style={{ border: "1px solid white" }}>
           {Data.length > 0 ? (
             <>
@@ -57,41 +80,55 @@ const DataTable = ({ actualData, fields, editableFields, delete_API, action, edi
                       <th
                         key={title}
                         scope="col"
-                        style={{ backgroundColor: "#3D3D3D", padding: "15px", color: "white" }}
+                        style={{
+                          backgroundColor: "#3D3D3D",
+                          padding: "15px",
+                          color: "white",
+                        }}
                       >
                         {title}
                       </th>
                     );
                   })}
-                  <th style={{ backgroundColor: "#3D3D3D", padding: "15px", color: "white" }}>
+                  <th
+                    style={{
+                      backgroundColor: "#3D3D3D",
+                      padding: "15px",
+                      color: "white",
+                    }}
+                  >
                     Action
                   </th>
                 </tr>
               </thead>
               <tbody>
                 {/* Displaying Data */}
-                {Data.map((elem, index) => {
+                {Data.filter(
+                  (elem, index) =>
+                    elem &&
+                    elem.username.toLowerCase().startsWith(query.toLowerCase())
+                ).map((elem, index) => {
                   return (
                     <tr key={index}>
                       {/* Map data through Fields so that every field gets mapped even though response data doesh't contain them */}
                       {fields.map((keys) => {
                         return (
-                          <td
-                            key={keys}
-                            style={{ backgroundColor: "#5E5E5E" }}
-                          >
-                            {elem[keys] ? 
-                            elem[keys] : "No Entry"}
+                          <td key={keys} style={{ backgroundColor: "#5E5E5E" }}>
+                            {elem[keys] ? elem[keys] : "No Entry"}
                           </td>
                         );
                       })}
                       <td style={{ backgroundColor: "#5E5E5E" }}>
-                         <AiTwotoneEdit className="fa-solid fa-pen-to-square mx-2" style={{color: '#63C5CB'}} onClick={(e) => edit_func(e, elem)} />  
+                        <AiTwotoneEdit
+                          className="fa-solid fa-pen-to-square mx-2"
+                          style={{ color: "#63C5CB" }}
+                          onClick={(e) => edit_func(e, elem)}
+                        />
                         <AiFillDelete
                           className="fas fa-trash mx-2"
                           style={{ color: "#FF7272" }}
                           onClick={(e) => delete_func(e, elem)}
-                         />
+                        />
                       </td>
                     </tr>
                   );
